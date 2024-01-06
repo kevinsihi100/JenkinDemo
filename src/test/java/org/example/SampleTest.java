77bf52bb54eaf4253bab0d78d5cb16e79175c54c
thara <kevinsihi100@gmail.com>
2024-01-06 13:46:23 +0000

    Added  3 test cases

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -1,9 +1,21 @@
+package org.example;
+
 import org.testng.annotations.Test;
 
 public class SampleTest {
     //create a test method
    @Test
-    public void test() {
-        System.out.println("Hello Word");
+    public void test1() {
+        System.out.println("Test Case1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test Case2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Test Case3");
     }
 }
